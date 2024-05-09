8277f19ef5b078a181a5c7c4a744e173ff98ab1b
Joan Bødker Pedersen <joan@joanbp.dk>
2024-05-09 18:24:12 +0200

    Created a new branch to play around with my own ideas without messing too much with the basic framework.
    Added a number of new voting algorithms to Vote.py.
    
    Changed all voting algorithms to output a dictionary
    of the candidates' scores, rather than simply a
    single winning candidate.

--- Testdata.py
+++ Testdata.py
@@ -15,8 +15,8 @@ CANDIDATES = {
 # Each voter has voted for a specific candidate. Different voters have different proven qualifications.
 # One voter in the sample is a fellowship candidate. Should they be allowed to vote?
 voter_sample_1 = [
-    Voter(CANDIDATES["C1"], []),
-    Voter(CANDIDATES["C1"], [NFT.FELLOWSHIP_COMM, NFT.FUNDA_1, NFT.FUNDA_2, NFT.FUNDA_3, NFT.FUNDA_4, NFT.FUNDA_4, NFT.FUNDA_5, NFT.FUNDA_LEGACY]),
+    Voter(CANDIDATES["C4"], []),
+    Voter(CANDIDATES["C2"], [NFT.FELLOWSHIP_COMM, NFT.FUNDA_1, NFT.FUNDA_2, NFT.FUNDA_3, NFT.FUNDA_4, NFT.FUNDA_4, NFT.FUNDA_5, NFT.FUNDA_LEGACY]),
     Voter(CANDIDATES["C2"], [], isCandidate = True),
     Voter(CANDIDATES["C3"], [NFT.FUNDA_1]),
     Voter(CANDIDATES["C4"], [NFT.FUNDA_1, NFT.FUNDA_2]),
@@ -24,7 +24,7 @@ voter_sample_1 = [
     Voter(CANDIDATES["C1"], [NFT.STUDY_SEASON_REGISTRATION, NFT.LIVE_TRACK_4, NFT.WONDERVERSE_TOP50]),
     Voter(CANDIDATES["C4"], [NFT.STUDY_SEASON_REGISTRATION]),
     Voter(CANDIDATES["C2"], [NFT.STUDY_SEASON_REGISTRATION, NFT.LIVE_TRACK_1, NFT.LIVE_TRACK_2]),
-    Voter(CANDIDATES["C4"], []),
+    Voter(CANDIDATES["C4"], [NFT.LIVE_TRACK_4, NFT.LIVE_TRACK_5]),
 ]
 
 def getSample(sampleID = 1):
--- Vote.py
+++ Vote.py
@@ -1,4 +1,5 @@
 import Testdata
+from Definitions import *
 
 CANDIDATES = Testdata.CANDIDATES
 
@@ -12,11 +13,89 @@ def popularity_contest(sample):
     for k in CANDIDATES.keys():
         votecount[k] = len([voter for voter in sample if voter.vote == CANDIDATES[k]])
     
-    return max(votecount, key = votecount.get)
+    return votecount
+
+def ask_the_dictator(sample):
+
+    (dictator, dictator_points) = (None, 0)
+
+    # Assign expertise points based on voters' NFTs and appoint the voter with most points as dictator
+    for voter in sample:
+        points = 0
+        for nft in voter.nfts:
+            if nft in [NFT.FELLOWSHIP_COMM]: points += 50
+            elif nft in [NFT.ETHCC_TE_2023_BARCAMP_SPEAKER, NFT.STUDY_SEASON_SPEAKER]: points += 10
+            elif nft in iter(NFT): points += 1
+
+        if points > dictator_points:
+            dictator = voter
+            dictator_points = points
+
+    cpoints = {c: 0 for c in CANDIDATES}
+    cpoints[dictator.vote.id] = 100
+
+    return cpoints
+
+def ask_the_experts(sample):
+
+    experts = set()
+    for voter in sample:
+        for nft in voter.nfts:
+            if nft in [NFT.FELLOWSHIP_COMM, NFT.ETHCC_TE_2023_BARCAMP_SPEAKER, NFT.STUDY_SEASON_SPEAKER]:
+                experts.add(voter)
+    
+    print(experts)
+    
+    return popularity_contest(experts)
+
+def ask_the_theoreticians(sample):
+    
+    cpoints = {c: 0 for c in CANDIDATES}
+
+    for voter in sample:
+        weight = 0
+        for nft in voter.nfts:
+            if nft in [NFT.FELLOWSHIP_COMM, NFT.ETHCC_TE_2023_BARCAMP_SPEAKER, NFT.STUDY_SEASON_SPEAKER, 
+                       NFT.FUNDA_1, NFT.FUNDA_2, NFT.FUNDA_3, NFT.FUNDA_4, NFT.FUNDA_5, NFT.FUNDA_LEGACY]:
+                weight += 1
+        cpoints[voter.vote.id] += weight
+
+    return cpoints
+
+def ask_the_participants(sample):
+
+    cpoints = {c: 0 for c in CANDIDATES}
+
+    for voter in sample:
+        weight = 0
+        for nft in voter.nfts:
+            if nft in [NFT.LIVE_TRACK_1, NFT.LIVE_TRACK_2, NFT.LIVE_TRACK_3, NFT.LIVE_TRACK_4]:
+                weight += 1
+            elif nft in [NFT.LIVE_TRACK_5, NFT.LIVE_TRACK_6, NFT.LIVE_TRACK_7, NFT.LIVE_TRACK_8]:
+                weight += 5
+        cpoints[voter.vote.id] += weight
+
+    return cpoints
+
+def points_to_percentages(cpoints):
+
+    total = sum(cpoints.values())
+    return {c: round(100 * cpoints[c] / total) for c in cpoints}
 
 
 # Run the test
 votes = Testdata.getSample()            # Fetch the list of votes - ie. voters with preferred candidates and personal qualifications
-winner = popularity_contest(votes)      # Run the voting algorithm
-print(winner)                           # Print the id of the winner
+
+dictator = points_to_percentages(ask_the_dictator(votes))
+experts = points_to_percentages(ask_the_experts(votes))
+theos = points_to_percentages(ask_the_theoreticians(votes))
+participants = points_to_percentages(ask_the_participants(votes))
+pop = points_to_percentages(popularity_contest(votes))
+
+print("Dictator: " + str(dictator))
+print("Experts: " + str(experts))
+print("Theoreticians: " + str(theos))
+print("Participants: " + str(participants))
+print("---")
+print("Everyone (popularity contest): " + str(pop))
 
